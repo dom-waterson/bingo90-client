2b9110e21c1d03e41530539b9448f1c2e5cf3b6e
Dominic Waterson <dominic.waterson@tombola.com>
2015-11-11 11:04:32 +0000

    got sample project working

---
+++ main-app/tests/sampleTest/sample-test.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+    describe('Sample Test', function () {
+
+        beforeEach(function () {
+
+        });
+
+        it('single sample test', function () {
+
+        });
+
+        afterEach(function () {
+
+        });
+    });
+})();
